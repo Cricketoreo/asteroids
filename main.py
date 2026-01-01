b6a692d4a2889a3197250d820821bea9b9d2d0da
Cricketoreo <zack.mctaggart@outlook.com>
2026-01-01 15:01:35 -0600

    B: player added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -8,16 +9,21 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
+        clock.tick(60)
+        dt += clock.get_time() / 1000.0
+       
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        
-
-
+    
 if __name__ == "__main__":
     main()
